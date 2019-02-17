6cb75990cd34ea84724e9637e889c328f11f5793
zealotTL <zealotTL@users.noreply.github.com>
2019-02-17 22:01:45 +0800

    vue稍作修改

--- king-demo/king-demo-vue/src/utils/request.js
+++ king-demo/king-demo-vue/src/utils/request.js
@@ -31,6 +31,7 @@ service.interceptors.response.use(
      * code为非20000是抛错 可结合自己业务进行修改
      */
     const res = response.data
+    console.log(res)
     if (res.code !== 20000) {
       Message({
         message: res.message,
