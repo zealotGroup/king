8af46c5ce8c0691be6f215897aa9b486019f3cfe
zealotTL <t953170224@163.com>
2019-05-19 11:22:00 +0800

    增加线程池

---
+++ king-core/king-core-zt/src/main/java/group/zealot/king/core/zt/async/AsyncConfig.java
@@ -0,0 +1,21 @@
+package group.zealot.king.core.zt.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+    @Bean
+    public AsyncConfigurer asyncConfigurer(ThreadPoolTaskExecutor threadPoolTaskExecutor) {
+        AsyncConfigurer asyncConfigurer = new AsyncConfigurer(){
+            public Executor getAsyncExecutor() {
+                return threadPoolTaskExecutor;
+            }
+        };
+        return asyncConfigurer;
+    }
+}
--- king-demo/king-demo-service/src/main/java/group/zealot/king/demo/service/Run.java
+++ king-demo/king-demo-service/src/main/java/group/zealot/king/demo/service/Run.java
@@ -1,12 +1,23 @@
 package group.zealot.king.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "group.zealot.king")
+@EnableScheduling
+@EnableAsync
 public class Run {
+    private static Logger logger = LoggerFactory.getLogger(Run.class);
+    public static ConfigurableApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        SpringApplication.run(Run.class, args);
+        logger.info("启动");
+        applicationContext = SpringApplication.run(Run.class, args);
+        logger.info("启动结束");
     }
 }
